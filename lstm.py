431057edc0769d73b64f50b915725e75f4275a66
no-execution <835437406@qq.com>
2018-07-23 01:20:56 -0500

    Add files via upload

--- lstm.py
+++ lstm.py
@@ -46,6 +46,8 @@ class net(nn.Module):
 		x,_ = self.lstm(x,(h0,c0))
 		
 		x = x[:,-1,:]
+		return x
+		'''
 		x = self.dropout(x)
 
 		#x = F.relu(self.fc1(x))
@@ -55,7 +57,7 @@ class net(nn.Module):
 		x = F.sigmoid(self.fc2(x))
 
 		return x
-
+        '''
 #读取.npy文件
 #把.npy文件得到的向量扔到DataLoader里进行预处理
 #输出DataLoader
@@ -71,7 +73,7 @@ def get_data(filename,batch_size):
 #forward计算output
 #计算损失以及相应的偏导数(梯度) backward
 #用optimizer进行参数更新 step
-def train(net,input_loader,label_loader,n_epochs=6):
+def train(net,input_loader,label_loader,n_epochs=10):
 	i = 0
 	for epoch in range(n_epochs):
 		total_loss = 0.0
@@ -87,7 +89,7 @@ def train(net,input_loader,label_loader,n_epochs=6):
 			#print(type(inputs[0][0][0].item()),type(labels[0].item()))
 
 			#定义优化器
-			optimizer = optim.Adam(net.parameters(),lr=0.001)
+			optimizer = optim.SGD(net.parameters(),lr=0.004)
 			optimizer.zero_grad()
 
 			#定义损失函数
@@ -124,12 +126,17 @@ def test(net,input_filename,label_filename,batch_size,k=10000):
 			print('accuracy of test_set k=',k,'is',100*n_right/k,'%')
 			break
 		inputs,labels = data
-		inputs = inputs.float()
+		inputs = inputs.long()
+		inputs = inputs.cuda()
 
 		out = net(inputs)
 		labels = labels.cuda()
 		#out = F.sigmoid(out)
-		outputs = torch.max(out,1)[1]
+		#outputs = torch.max(out,1)[1]  #对于输出为2维
+		outputs = torch.ge(out,0.51)
+		outputs = outputs.long()
+		outputs = outputs.view(batch_size)
+		#print(outputs,outputs.size(),labels,labels.size())
 		mid = outputs==labels
 		n_right += torch.sum(mid).item()
 
@@ -138,9 +145,61 @@ def test(net,input_filename,label_filename,batch_size,k=10000):
 
 def main():
 	label_loader = get_data('labels_200000.npy',4)
-	input_loader = get_data('train_pad_40dim.npy',4)
+	input_loader = get_data('train_pad_20dim.npy',4)
 	ls = net(300,4,128)
 	ls = ls.cuda()
 	train(ls,input_loader,label_loader)
 	return ls
 
+
+def predict(net,input_filename,label_filename,batch_size,k=2000000):
+	input_loader = get_data(input_filename,batch_size)
+	label_loader = get_data(label_filename,batch_size)
+	i,n_right = 0,0
+	res = []
+	for data in zip(input_loader,label_loader):
+		if i >= k :
+			print('accuracy of test_set k=',k,'is',100*n_right/k,'%')
+			break
+		inputs,labels = data
+		inputs = inputs.long()
+		inputs = inputs.cuda()
+
+		out = net(inputs)
+		#labels = labels.cuda()
+		#out = F.sigmoid(out)
+		#outputs = torch.max(out,1)[1]  #对于输出为2维
+		outputs = torch.ge(out,0.5)
+		outputs = outputs.long()
+		outputs = outputs.view(batch_size)
+		res.extend([x.item() for x in outputs])
+		#print(outputs,outputs.size(),labels,labels.size())
+		
+		#n_right += torch.sum(mid).item()
+
+		i += batch_size
+		if i % 2000 == 0:
+			print('doing',i)
+	return res
+
+
+#该函数的作用是，拿到lstm层的输出，用以进行semi-supervised learning或者拟合等操作
+def get_res_of_lstm(net,input_filename,batch_size,k=2000000):
+	input_loader = get_data(input_filename,batch_size)
+	for i,data in enumerate(input_loader):
+		inputs = data
+		inputs = inputs.long()
+		inputs = inputs.cuda()
+
+		out = net(inputs)
+		if i == 0:
+			res = out.cpu().detach().numpy()
+			continue
+		res = np.concatenate((res,out.cpu().detach().numpy()),axis=0)
+		if i % 1000 == 999:
+			print('finished',i+1)
+	return res
+
+
+#net.load_state_dict(torch.load('net_1_out_300_4_128_20dim_10epoch.pkl'))
+#test
